66accba3748eccc94daf92e90aaf3d76f46ed5ad
Arnaud Kervern <akervern@nuxeo.com>
2020-10-28 14:52:34 +0100

    NXCLI-50: Add Docker generated module

--- nuxeo-customer-project-sample-core/src/main/java/com/customer/sample/service/SampleServiceImpl.java
+++ nuxeo-customer-project-sample-core/src/main/java/com/customer/sample/service/SampleServiceImpl.java
@@ -3,6 +3,7 @@ package com.customer.sample.service;
 import org.nuxeo.runtime.model.ComponentContext;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.model.DefaultComponent;
+import org.nuxeo.runtime.model.Extension;
 
 public class SampleServiceImpl extends DefaultComponent implements SampleService {
 
@@ -31,26 +32,43 @@ public class SampleServiceImpl extends DefaultComponent implements SampleService
     }
 
     /**
-     * Application started notification.
-     * Called after the application started.
-     * You can do here any initialization that requires a working application
-     * (all resolved bundles and components are active at that moment)
+     * Registers the given extension.
      *
-     * @param context the component context. Use it to get the current bundle context
-     * @throws Exception
+     * @param extension the extension to register
      */
     @Override
-    public void applicationStarted(ComponentContext context) {
+    public void registerExtension(Extension extension) {
+        super.registerExtension(extension);
+    }
+
+     /**
+     * Unregisters the given extension.
+     *
+     * @param extension the extension to unregister
+     */
+    @Override
+    public void unregisterExtension(Extension extension) {
+        super.unregisterExtension(extension);
+    }
+
+    /**
+     * Start the component. This method is called after all the components were resolved and activated
+     *
+     * @param context the component context. Use it to get the current bundle context
+     */
+    @Override
+    public void start(ComponentContext context) {
         // do nothing by default. You can remove this method if not used.
     }
 
+    /**
+     * Stop the component.
+     *
+     * @param context the component context. Use it to get the current bundle context
+     * @throws InterruptedException
+     */
     @Override
-    public void registerContribution(Object contribution, String extensionPoint, ComponentInstance contributor) {
-        // Add some logic here to handle contributions
-    }
-
-    @Override
-    public void unregisterContribution(Object contribution, String extensionPoint, ComponentInstance contributor) {
-        // Logic to do when unregistering any contribution
+    public void stop(ComponentContext context) throws InterruptedException {
+        // do nothing by default. You can remove this method if not used.
     }
 }
